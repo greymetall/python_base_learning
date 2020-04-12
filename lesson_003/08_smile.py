3586483da0c77f26b9dfd98a130236ed152256d9
Andrey Cholokyan <cholokyan85a@gmail.com>
2020-04-12 15:58:43 +0300

    lesson_003/08_smile.py: теперь начальная точка задаётся рандомом, а координаты x,y берутся из внутренних координат функции sd.random_point()

--- lesson_003/08_smile.py
+++ lesson_003/08_smile.py
@@ -11,39 +11,49 @@ sd.background_color = (245, 240, 240)
 # Параметры функции: кордината X, координата Y, цвет.
 # Вывести 10 смайликов в произвольных точках экрана.
 
-def smile(X, Y, color):
+def smile(point, color):
     # голова смайла
-    point = sd.get_point(X, Y)
+    x = point.x
+    y = point.y
+    if x < 90:
+        x = 90
+    elif point.x > 1120:
+        x = 1120
+    if y < 50:
+        y = 50
+    elif y > 500:
+        y = 500
+    # point = sd.get_point(x, y)
     random_delta_x = sd.random_number(50, 100)
     random_delta_y = sd.random_number(40, random_delta_x - 10)
-    left_bottom = sd.get_point(x=X - random_delta_x, y=Y - random_delta_y)
-    right_top = sd.get_point(x=X + random_delta_x, y=Y + random_delta_y)
+    left_bottom = sd.get_point(x=x - random_delta_x, y=y - random_delta_y)
+    right_top = sd.get_point(x=x + random_delta_x, y=y + random_delta_y)
     color = sd.random_color()
     sd.ellipse(left_bottom, right_top, color=color, width=2)
 
     # глаза
-    center_left_eye = sd.get_point(x=X-random_delta_x+sd.random_number(30, 40),
-                                   y=Y+random_delta_y-sd.random_number(30, 35))
+    center_left_eye = sd.get_point(x=x-random_delta_x+sd.random_number(30, 40),
+                                   y=y+random_delta_y-sd.random_number(30, 35))
     radius_left_eye = sd.random_number(7, 10)
     # width_left_eye = sd.random_number(2, 4) * sd.random_number(0, 1)
-    center_right_eye = sd.get_point(x=X+random_delta_x-sd.random_number(30, 40),
-                                   y=Y+random_delta_y-sd.random_number(30, 35))
+    center_right_eye = sd.get_point(x=x+random_delta_x-sd.random_number(30, 40),
+                                   y=y+random_delta_y-sd.random_number(30, 35))
     radius_right_eye = sd.random_number(7, 10)
     # widht_right_eye = sd.random_number(2, 4) * sd.random_number(0, 1)
     sd.circle(center_position=center_left_eye, radius=radius_left_eye, color=color, width=sd.random_number(0,2))
     sd.circle(center_position=center_right_eye, radius=radius_right_eye, color=color, width=sd.random_number(0,2))
 
     # рот
-    point1 = sd.get_point(x=X - random_delta_x + sd.random_number(20, 40),
-                          y=Y - random_delta_y + sd.random_number(30, 40))
-    point2 = sd.get_point(x=X - random_delta_x + sd.random_number(40, 50),
-                          y=Y - random_delta_y + sd.random_number(15, 25))
-    point3 = sd.get_point(x=X + sd.random_number(-10, 10),
-                          y=Y - random_delta_y + sd.random_number(20, 30))
-    point4 = sd.get_point(x=X + random_delta_x - sd.random_number(40, 50),
-                          y=Y - random_delta_y + sd.random_number(15, 25))
-    point5 = sd.get_point(x=X + random_delta_x - sd.random_number(20, 40),
-                          y=Y - random_delta_y + sd.random_number(30, 45))
+    point1 = sd.get_point(x=x - random_delta_x + sd.random_number(20, 40),
+                          y=y - random_delta_y + sd.random_number(30, 40))
+    point2 = sd.get_point(x=x - random_delta_x + sd.random_number(40, 50),
+                          y=y - random_delta_y + sd.random_number(15, 25))
+    point3 = sd.get_point(x=x + sd.random_number(-10, 10),
+                          y=y - random_delta_y + sd.random_number(20, 30))
+    point4 = sd.get_point(x=x + random_delta_x - sd.random_number(40, 50),
+                          y=y - random_delta_y + sd.random_number(15, 25))
+    point5 = sd.get_point(x=x + random_delta_x - sd.random_number(20, 40),
+                          y=y - random_delta_y + sd.random_number(30, 45))
 
     points = [point1, point2, point3, point4, point5]
     random_points_list1 = points[0:sd.random_number(2, 5)]
@@ -57,6 +67,7 @@ def smile(X, Y, color):
 
 
 for _ in range(10):
-    smile(X=sd.random_number(70,1140), Y=sd.random_number(50,550), color=None)
+    # smile(X=sd.random_number(70,1140), Y=sd.random_number(50,550), color=None)
+    smile(point=sd.random_point(), color=None)
 
 sd.pause()
