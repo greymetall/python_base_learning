dc6b96dd40b512b1b9a1fbe4e49e7358a97a756e
Andrey Cholokyan <cholokyan85a@gmail.com>
2020-04-14 01:50:40 +0300

    lesson_003/06_rainbow.py: упростил код (по-пайтоновски!): цикл for бежит сразу по списку rainbow_colors переменной color без лишних переменных и range(7)

--- lesson_003/06_rainbow.py
+++ lesson_003/06_rainbow.py
@@ -11,11 +11,10 @@ rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN
 
 # Нарисовать радугу: 7 линий разного цвета толщиной 4 с шагом 5 из точки (50, 50) в точку (350, 450)
 # step = 0
-# for i in range(7):
+# for color in rainbow_colors:
 #     start_point = sd.get_point(50, 300-step)
 #     end_point = sd.get_point(850, 650-step)
 #     step += 30
-#     color = rainbow_colors[i]
 #     sd.line(start_point=start_point, end_point=end_point, color=color, width = 30)
 
 # Усложненное задание, делать по желанию.
@@ -23,9 +22,8 @@ rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN
 # поэкспериментировать с параметрами, что бы было красиво
 
 step = 0
-for i in range(7):
+for color in rainbow_colors[::-1]:
     center = sd.get_point(715, -500)
-    color = rainbow_colors[::-1][i]
     sd.circle(center_position=center, radius=1050-step, color=color, width=30)
     step += 30
 sd.pause()
