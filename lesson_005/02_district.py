d90fd0c3b227a027ac7c3841105f4b26434a2546
Andrey Cholokyan <cholokyan85a@gmail.com>
2020-04-26 17:58:43 +0300

    lesson_005/02_district.py: д/з выполнено

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -5,8 +5,16 @@
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
-# TODO здесь ваш код
 
-
+from district.central_street.house1 import room1 as csh1r1, room2 as csh1r2
+from district.central_street.house2 import room1 as csh2r1, room2 as csh2r2
+from district.soviet_street.house1 import room1 as ssh1r1, room2 as ssh1r2
+from district.soviet_street.house2 import room1 as ssh2r1, room2 as ssh2r2
+list_folks = [csh1r1.folks, csh1r2.folks, csh2r1.folks, csh2r2.folks, ssh1r1.folks,
+             ssh1r2.folks, ssh2r1.folks, ssh2r2.folks]
+all_folks = []
+for folks in list_folks:
+    all_folks.extend(folks)
+print('На районе живут:', ', '.join(all_folks))
 
 
