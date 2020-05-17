8c41780ba95d9849abd8f4630d79f7af7e9e0a8e
Andrey Cholokyan <cholokyan85a@gmail.com>
2020-05-17 10:53:11 +0300

    lesson_010: 03_registration_log.py: сделал д/з, стабильная версия + оптимизировал код

--- lesson_010/03_registration_log.py
+++ lesson_010/03_registration_log.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Есть файл с протоколом регистраций пользователей на сайте - registrations.txt
+# Есть файл с протоколом регистраций пользователей на сайте - registrations_.txt
 # Каждая строка содержит: ИМЯ ЕМЕЙЛ ВОЗРАСТ, разделенные пробелами
 # Например:
 # Василий test@test.ru 27
@@ -22,4 +22,66 @@
 # - поле возраст НЕ является числом от 10 до 99: ValueError
 # Вызов метода обернуть в try-except.
 
-# TODO здесь ваш код
+
+class NotNameError(Exception):
+    pass
+
+
+class NotEmailError(Exception):
+    pass
+
+
+def tester_files(line):
+    # print(f'Read line {line}', flush=True)
+    length = len(line.split())
+    if length < 3:
+        raise ValueError(f'Недостаточно данных. Обнаружено: {length} требуетя: 3')
+    elif length > 3:
+        raise ValueError(f'Данных больше, чем нужно. Обнаружено: {length} требуетя: 3')
+    else:
+        name, email, age = line.split(' ')
+    if not name.isalpha():
+        raise NotNameError('Имя содержит недопустимые символы')
+    if '@' not in email and '.' not in email:
+        raise NotEmailError('Несуществующий email')
+    if age.isnumeric():
+        if not 10 <= int(age) <= 99:
+            raise ValueError('Возраст вне диапазона от 10 до 99')
+    else:
+        raise ValueError('Возраст не распознан')
+
+
+good_log = open('registrations_good.log', 'w', encoding='utf8')
+bad_log = open('registrations_bad.log', 'w', encoding='utf8')
+with open('registrations.txt', 'r', encoding='utf8') as ff:
+    for number, string in enumerate(ff, 1):
+        number, string = str(number) + '.', string[:-1]
+        output = number + string + ' - error: '
+        try:
+            tester_files(line=string)
+        except ValueError as exc:
+            bad_log.write(output + exc.args[0] + '\n')
+            print(output + exc.args[0])
+        except NotNameError as exc:
+            bad_log.write(output + exc.args[0] + '\n')
+            print(output + exc.args[0])
+        except NotEmailError as exc:
+            bad_log.write(output + exc.args[0] + '\n')
+            print(output + exc.args[0])
+        except Exception as exc:
+            bad_log.write(output + exc.args[0] + '\n')
+            print(output + 'Неизвестная ошибка:', exc.args[0])
+        else:
+            good_log.write(number + string + '\n')
+good_log.close()
+bad_log.close()
+
+
+# import os
+# os.chdir(r'D:\Курсы\Skillbox\Основы_Python\python_homeworks\lesson_010')
+# print(os.getcwd())
+# print(os.listdir(os.getcwd()))
+# print(os.path.abspath('registrations_.txt'))
+# print(os.path.abspath('registrations_.txt'))
+# print(os.getcwd())
+# os.defpath(registrations_.txt)
